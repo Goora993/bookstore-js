c902f7ab4ec867616c816c4ae51e3646801c3430
Goora993 <ozzyallegropl@gmail.com>
2022-12-19 13:28:25 +0100

    Move API logic to controller class. Add validation and error codes to endpoints.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Book = require('../models/book/book');
+const bookController = require('../controller/bookController')
 
 const router = express.Router();
 
@@ -9,71 +9,18 @@ router.get('/test', (req, res) => {
 })
 
 // Get all books from the store
-router.get('/books', async(req, res) => {
-    try{
-        const data = await Book.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get('/books', bookController.getAllBooks)
 
 // Get specific book by ID from the store
-router.get('/book/:id', async (req, res) => {
-    try{
-        const data = await Book.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get('/book/:id', bookController.getBookById)
 
 // Add new book to the store
-router.post('/book/add', async (req, res) => {
-    const data = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        price: req.body.price,
-        quantity: req.body.quantity
-    });
-
-    data.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => console.log(err));
-})
+router.post('/book/add', bookController.addBook)
 
 // Update book by ID
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-
-        const result = await Book.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+router.patch('/update/:id', bookController.updateBookById)
 
 // Delete book by ID
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Book.findByIdAndDelete(id)
-        res.send(`Document with ${data.title} has been deleted.`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+router.delete('/delete/:id', bookController.deleteBookById)
 
 module.exports = router;
