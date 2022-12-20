596fd4362529517cb36c145e4c92daa3c3ab40ff
Goora993 <ozzyallegropl@gmail.com>
2022-12-20 16:58:00 +0100

    Little fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.listen(process.env.PORT);
 //Middleware & static
 app.use(express.static('public'));
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json);
 
 //Book routes
 app.use(bodyParser.json());
